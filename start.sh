727a8e987d0f93347bd5875ffdcfc5dde34f1170
unknown <brian@5CD101G3B0.gdoa.com>
2023-07-13 18:25:23 +0800

    u

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ port=8083
 
 git pull
 
-netstat -ntpl|grep :${port}|awk '{print $7}'|awk -F'/' '{print $1}'
+netstat -ntpl|grep :${port}|awk '{print $7}'|awk -F'/' '{print $1}'|xargs kill -9 
 export PORT=${port} && nohup go run main.go > nohup.log &
