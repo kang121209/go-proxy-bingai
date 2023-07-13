9ab3dbdb95b94ae9ced45d2a09dbab214d20a8a1
unknown <brian@5CD101G3B0.gdoa.com>
2023-07-13 17:22:55 +0800

    u

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ port=8083
 git pull
 
 ps -ef|grep ":${port}"|grep -v 'grep'|awk '{print $2}'|xargs kill -9
-export PORT=${port} && go run main.go
+export PORT=${port} && nohup go run main.go > nohup.log &
